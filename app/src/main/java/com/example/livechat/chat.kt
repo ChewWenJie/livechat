517d32b391a412229b2cd08aeaf4d2be056437c0
HP <chewwj-pm17@student.tarc.edu.my>
2020-01-09 01:27:36 +0800

    coomitt

--- app/src/main/java/com/example/livechat/chat.kt
+++ app/src/main/java/com/example/livechat/chat.kt
@@ -2,6 +2,10 @@ package com.example.livechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class chat : AppCompatActivity() {
 
@@ -10,5 +14,36 @@ class chat : AppCompatActivity() {
         setContentView(R.layout.activity_chat)
 
         supportActionBar?.title = "Chat"
+
+        val adapter = GroupAdapter<ViewHolder>()
+        adapter.add(ChatfromItem())
+        adapter.add(ChattoItem())
+        adapter.add(ChatfromItem())
+        adapter.add(ChattoItem())
+        adapter.add(ChatfromItem())
+        adapter.add(ChattoItem())
+        adapter.add(ChatfromItem())
+        adapter.add(ChattoItem())
+
+        recycleview_chat.adapter = adapter
+    }
+}
+
+class ChatfromItem: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.chat_from_row
+    }
+}
+class ChattoItem: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.chat_too_row
     }
 }
