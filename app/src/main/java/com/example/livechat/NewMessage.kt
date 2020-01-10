8bd24e452ae3cfc0ee1b3d4304f82a0b76dbd685
HP <chewwj-pm17@student.tarc.edu.my>
2020-01-10 11:14:43 +0800

    coomitt

--- app/src/main/java/com/example/livechat/NewMessage.kt
+++ app/src/main/java/com/example/livechat/NewMessage.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
+import com.example.livechat.latest_activity.Companion.currentUser
 import com.example.livechat.models.User
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -19,18 +21,20 @@ import kotlinx.android.synthetic.main.user_row_message.view.*
 import java.text.FieldPosition
 
 class NewMessage : AppCompatActivity() {
-
+    companion object{
+        val USER_KEY = "USER_KEY"
+    }
+    var toUser: User? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+        toUser = intent.getParcelableExtra<User>(NewMessage.USER_KEY)
 
         supportActionBar?.title = "Select User"
 
         getUsers()
     }
-    companion object{
-        val USER_KEY = "USER_KEY"
-    }
+
     private fun getUsers() {
         val ref = FirebaseDatabase.getInstance().getReference("/users")
         ref.addListenerForSingleValueEvent(object: ValueEventListener {
@@ -39,9 +43,14 @@ class NewMessage : AppCompatActivity() {
                 val adapter = GroupAdapter<ViewHolder>()
 
                 p0.children.forEach {
+//                    currentUser = p0.getValue(User::class.java)
+                    val currentUser = FirebaseAuth.getInstance().currentUser?.uid
                     val user = it.getValue(User::class.java)
                     if (user != null) {
-                        adapter.add(UserItem(user))
+                        if (user.uid != currentUser) {
+                            adapter.add(UserItem(user))
+                        }
+
                     }
                 }
                 adapter.setOnItemClickListener { item, view ->
