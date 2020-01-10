d61195a15bed49c76f0ff9db878732bf7773fb38
HP <chewwj-pm17@student.tarc.edu.my>
2020-01-10 11:35:56 +0800

    coomitt

---
+++ app/src/main/java/com/example/livechat/view/LatestMessageRows.kt
@@ -0,0 +1,44 @@
+package com.example.livechat.view
+
+import com.example.livechat.R
+import com.example.livechat.models.ChatMessage
+import com.example.livechat.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.latest_message_row.view.*
+
+class LatestMessageRow(val chatMessage: ChatMessage): Item<ViewHolder>() {
+    var chatPartnerUser: User? = null
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.txtView_latestmessages.text = chatMessage.text
+
+        val chatPartnerId: String
+        if (chatMessage.fromId == FirebaseAuth.getInstance().uid) {
+            chatPartnerId = chatMessage.toId
+        } else {
+            chatPartnerId = chatMessage.fromId
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                chatPartnerUser = p0.getValue(User::class.java)
+                viewHolder.itemView.txtview_latest.text = chatPartnerUser?.username
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.latest_message_row
+    }
+}
